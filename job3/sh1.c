25400a347538af26797768cc1489e4eeb96a4ecb
yym68686 <yym68686@outlook.com>
2022-03-14 21:11:14 +0000

    sh1.c 完成 echo

--- job3/sh1.c
+++ job3/sh1.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 void mysys(char *command)
 {
+	if (strcmp(command, "exit") == 0) {
+		exit(0);
+	}
     pid_t pid;
     pid = fork();
     if (pid == 0){
-        char str[11111];
-        strncpy(str, command, strlen(command) + 1);
-        char *p, *argv[11111];
+		char *str = (char *)malloc(sizeof(command));
+	    strcpy(str, command);
+		char *p, *argv[11111];
         int i = 0, error = -1;
-        p = strtok(str, " ");
-        while(p != NULL ) {
+		p = strtok(str, " ");
+        while(p != NULL) {
             argv[i++] = p;
-            p = strtok(NULL, str);
+            p = strtok(NULL, " ");
         }
         argv[i] = NULL;
-        error = execvp(argv[0], argv);
+		error = execvp(argv[0], argv);
         if (error == -1) {
             puts("exec error!");
             return;
         }
-        puts("");
     }
     wait(NULL);
 }
 
 int main()
 {
-    char s[11111];
-    do{
-        printf("> ");
-        scanf("%[^\n]", s);
-        mysys(s);
-    }while (s != "exit");
+	char command[11111];
+	int count;
+    while (1){
+		write(1, "> ", sizeof("> "));
+		count = read(0, command, sizeof(command));
+		command[count - 1] = 0;
+        mysys(command);
+    }
     return 0;
 }
