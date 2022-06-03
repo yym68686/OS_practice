ddb142d595130c252380e200640c47a81b2c86b6
yym68686 <yym68686@outlook.com>
2022-06-03 06:33:07 +0000

    完成 job8/pc.c

--- job8/pc.c
+++ job8/pc.c
@@ -17,34 +17,34 @@ int buffer2_is_empty(){
 }
 
 int buffer1_is_full(){
-    return (in1 + 1) % CAPACITY == out1;
+    return (in1 + 1) % CAPACITY1 == out1;
 }
 int buffer2_is_full(){
-    return (in2 + 1) % CAPACITY == out2;
+    return (in2 + 1) % CAPACITY2 == out2;
 }
 
 int get_item1(){
     int item;
     item = buffer1[out1];
-    out2 = (out2 + 1) % CAPACITY;
+    out1 = (out1 + 1) % CAPACITY1;
     return item;
 }
 
 int get_item2(){
     int item;
     item = buffer2[out2];
-    out2 = (out2 + 1) % CAPACITY;
+    out2 = (out2 + 1) % CAPACITY2;
     return item;
 }
 
 void put_item1(int item){
     buffer1[in1] = item;
-    in1 = (in1 + 1) % CAPACITY;
+    in1 = (in1 + 1) % CAPACITY1;
 }
 
 void put_item2(int item){
     buffer2[in2] = item;
-    in2 = (in2 + 1) % CAPACITY;
+    in2 = (in2 + 1) % CAPACITY2;
 }
 
 pthread_mutex_t mutex1;
@@ -54,14 +54,13 @@ pthread_mutex_t mutex2;
 pthread_cond_t wait_empty_buffer2;
 pthread_cond_t wait_full_buffer2;
 
-#define ITEM_COUNT (CAPACITY * 2)
+#define ITEM_COUNT (CAPACITY1 * 2)
 
 void *consume(void *arg)
 {
-    int i;
     int item;
 
-    for (i = 0; i < ITEM_COUNT; i++) { 
+    for (int i = 0; i < ITEM_COUNT; i++) { 
         pthread_mutex_lock(&mutex2);
         while (buffer2_is_empty())
             pthread_cond_wait(&wait_full_buffer2, &mutex2);
@@ -75,7 +74,8 @@ void *consume(void *arg)
     return NULL;
 }
 void *computer(void *arg){
-    for (i = 0; i < ITEM_COUNT; i++) { 
+    int item;
+    for (int i = 0; i < ITEM_COUNT; i++) { 
         pthread_mutex_lock(&mutex1);
         while (buffer1_is_empty()) 
             pthread_cond_wait(&wait_full_buffer1, &mutex1);
@@ -89,7 +89,7 @@ void *computer(void *arg){
         while (buffer2_is_full()) 
             pthread_cond_wait(&wait_empty_buffer2, &mutex2);
         put_item2(item);
-        printf("computer put item: %c\n", item); 
+        printf("    computer put item: %c\n", item); 
         pthread_cond_signal(&wait_full_buffer2);
         pthread_mutex_unlock(&mutex2);
     }
@@ -98,10 +98,9 @@ void *computer(void *arg){
 
 void *produce(void *arg)
 {
-    int i;
     int item;
 
-    for (i = 0; i < ITEM_COUNT; i++) { 
+    for (int i = 0; i < ITEM_COUNT; i++) { 
         pthread_mutex_lock(&mutex1);
         while (buffer1_is_full()) 
             pthread_cond_wait(&wait_empty_buffer1, &mutex1);
